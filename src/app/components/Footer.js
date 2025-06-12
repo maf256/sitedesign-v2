3390b17378f2313c48ef68909bb1e45201c9eb44
Majid <maf256@gmail.com>
2025-06-12 09:18:14 +0330

    styled landing section initially

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,5 +1,5 @@
 import "../scss/footer.scss";
 
 export default function Footer() {
-  return <footer> hi </footer>;
+  return <footer></footer>;
 }
--- src/app/components/LandingSection.js
+++ src/app/components/LandingSection.js
@@ -2,18 +2,24 @@ import Image from "next/image";
 import "../scss/landingSection.scss";
 export default function LandingSection() {
   return (
-    <section>
-      <h1 className="main-title">
-        <span>MAJID</span>
-        <Image src="/avatar.jpg" alt="Profile picture" width={59} height={59} />
-        <br />
-        <span> ASKARIFARSANGI</span>
-      </h1>
+    <section className="landing-section">
+      <div className="landing-content">
+        <h1 className="main-title">
+          <div className="title-line-one">
+            <span>MAJID</span>
+            <Image src="/avatar.jpg" alt="Profile picture" width={96} height={96} />
+          </div>
+          <span> ASKARIFARSANGI</span>
+        </h1>
 
-      <p>
-        I am a software engineer and web developer,I am happy to have a digital
-        chat or meet over a cup of coffee.
-      </p>
-    </section>
+        <p>
+          I am a <span>software</span>  engineer and <span> web </span> developer,<br />I am happy to have a digital chat or meet over a cup of coffee.
+        </p>
+
+      </div>
+      <span className="arrow-down">
+        ↓
+      </span >
+    </section >
   );
 }
