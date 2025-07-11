60589e9dfd39698501e7f72168cfd884b904784f
Majid <maf256@gmail.com>
2025-07-11 07:31:04 +0330

    fixed ribon bug not rendering expertise

--- src/app/components/Expertise.js
+++ src/app/components/Expertise.js
@@ -1,35 +1,6 @@
-import { useState, useEffect } from "react";
 import "../scss/expertise.scss";
-import {
-  GraduationCap,
-  Calendar,
-  Code,
-  Database,
-  Users,
-  Settings,
-} from "lucide-react";
+import { Code, Database, Users, Settings } from "lucide-react";
 
-export default function Expertise() {
-  return (
-    <div id="expertise">
-      <div>
-        Expertise
-      </div>
-      <br />
-      <div>
-        Expertise
-      </div>
-      <br />
-      <div>
-        Expertise
-      </div>
-      <br />
-      <div>
-        Expertise
-      </div></div>
-  )
-}
-/*
 export default function Expertise() {
   const skillCategories = [
     {
@@ -107,51 +78,6 @@ export default function Expertise() {
     },
   ];
 
-  const education = [
-    {
-      period: "2013 - 2015",
-      degree: "Master in Business Strategic",
-      institution: "PNU University of IRAN",
-      type: "degree",
-    },
-    {
-      period: "2011 - 2013",
-      degree: "Bachelor in Computer Engineering",
-      institution: "AZAD University of IRAN",
-      type: "degree",
-    },
-    {
-      period: "2001 - 2004",
-      degree: "Associate in Computer Science",
-      institution: "AZAD University of IRAN",
-      type: "degree",
-    },
-    {
-      period: "2024",
-      degree: "Kotlin for Beginners",
-      institution: "Udemy",
-      type: "course",
-    },
-    {
-      period: "2024",
-      degree: "Project Management",
-      institution: "Holte Academy",
-      type: "course",
-    },
-    {
-      period: "2022",
-      degree: "Next.js & React Complete Guide",
-      institution: "Udemy",
-      type: "course",
-    },
-    {
-      period: "2021",
-      degree: "Web Developer Bootcamp",
-      institution: "Udemy",
-      type: "course",
-    },
-  ];
-
   return (
     <section className="expertise" id="expertise">
       <div className="expertise__container">
@@ -179,39 +105,8 @@ export default function Expertise() {
             </div>
           </div>
         </div>
-        <div className="expertise__education">
-          <div className="expertise__education-header">
-            <GraduationCap className="expertise__education-icon" />
-            <h3>Education</h3>
-          </div>
-
-          <div className="expertise__timeline">
-            {education.map((item, index) => (
-              <div
-                key={index}
-                className={`expertise__timeline-item expertise__timeline-item--${item.type}`}
-              >
-                <div className="expertise__timeline-marker"></div>
-                <div className="expertise__timeline-content">
-                  <div className="expertise__timeline-period">
-                    <Calendar className="expertise__timeline-calendar" />
-                    <span>{item.period}</span>
-                  </div>
-                  <h5 className="expertise__timeline-degree">
-                    {item.degree}
-                  </h5>
-                  <p className="expertise__timeline-institution">
-                    {item.institution}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
       </div>
     </section>
   );
-};
+}
 
-
-*/
